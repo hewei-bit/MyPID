b28d844e654f597608aaa5f0cde96bd4c06a1103
hewei-bit <1003826976@qq.com>
2021-02-05 11:18:09 +0800

    第二天

--- USER/main.c
+++ USER/main.c
@@ -10,21 +10,43 @@
 /************************************************
 PID 控制实验
 作者：何蔚
+IO引脚分配如下
+	MAX6675 
+		PD2 	---- 	SCK
+		PC12 	---- 	SO
+		PC11	----	CS
+		
+	AT24C02
+		PB6		----	IIC
+		PB7		----	IIC
+		
+	LED0 	----	PB5 
+	LED1	----	PE5
+	
+	KEY0~2	---- 	PE2~4
+	WK_UP	----	GPIOA.0
+		
+
 ************************************************/
 u16 Kms10;
 
 //要写入到24c02的字符串数组
-const u8 TEXT_Buffer[] = {"WarShipSTM32 IIC TEST"};
+const u8 TEXT_Buffer[] = {"PID TEST"};
 #define SIZE sizeof(TEXT_Buffer)
 
 int main(void)
 {
+	u16 i=0;
+	u8 datatemp[SIZE];
+	
     delay_init();
     LCD_Init();
     LED_Init();
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
     uart_init(115200);                              //串口初始化为115200
-
+	Max6675_Init();
+	AT24CXX_Write(0,(u8*)TEXT_Buffer,SIZE);
+	AT24CXX_Read(0,datatemp,SIZE);
     LCD_ShowString(30, 50, 200, 16, 16, "PID TEST");
 
     while (1)
@@ -33,57 +55,5 @@ int main(void)
 }
 
 #if 0
-	u8 key;
-	u16 i=0;
-	u8 datatemp[SIZE];
-	delay_init();	    	 //延时函数初始化	  
-  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
-	uart_init(115200);	 	//串口初始化为115200
-	LED_Init();		  		//初始化与LED连接的硬件接口
-	LCD_Init();			   	//初始化LCD 	
-	KEY_Init();				//按键初始化		 	 	
-	AT24CXX_Init();			//IIC初始化 
-
- 	POINT_COLOR=RED;//设置字体为红色 
-	LCD_ShowString(30,50,200,16,16,"WarShip STM32");	
-	LCD_ShowString(30,70,200,16,16,"IIC TEST");	
-	LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
-	LCD_ShowString(30,110,200,16,16,"2015/1/15");	
-	LCD_ShowString(30,130,200,16,16,"KEY1:Write  KEY0:Read");	//显示提示信息		
- 	while(AT24CXX_Check())//检测不到24c02
-	{
-		LCD_ShowString(30,150,200,16,16,"24C02 Check Failed!");
-		delay_ms(500);
-		LCD_ShowString(30,150,200,16,16,"Please Check!      ");
-		delay_ms(500);
-		LED0=!LED0;//DS0闪烁
-	}
-	LCD_ShowString(30,150,200,16,16,"24C02 Ready!");    
- 	POINT_COLOR=BLUE;//设置字体为蓝色	  
-	while(1)
-	{
-		key=KEY_Scan(0);
-		if(key==KEY1_PRES)//KEY_UP按下,写入24C02
-		{
-			LCD_Fill(0,170,239,319,WHITE);//清除半屏    
- 			LCD_ShowString(30,170,200,16,16,"Start Write 24C02....");
-			AT24CXX_Write(0,(u8*)TEXT_Buffer,SIZE);
-			LCD_ShowString(30,170,200,16,16,"24C02 Write Finished!");//提示传送完成
-		}
-		if(key==KEY0_PRES)//KEY1按下,读取字符串并显示
-		{
- 			LCD_ShowString(30,170,200,16,16,"Start Read 24C02.... ");
-			AT24CXX_Read(0,datatemp,SIZE);
-			LCD_ShowString(30,170,200,16,16,"The Data Readed Is:  ");//提示传送完成
-			LCD_ShowString(30,190,200,16,16,datatemp);//显示读到的字符串
-		}
-		i++;
-		delay_ms(10);
-		if(i==20)
-		{
-			LED0=!LED0;//提示系统正在运行	
-			i=0;
-		}		   
-	}
 
 #endif
